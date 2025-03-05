11391088cd73fffad844c85de37b07e60ea689e0
MatheusESouza21 <bmakgacha@gmail.com>
2025-03-05 19:29:55 -0300

    login

--- index.py
+++ index.py
@@ -1,28 +1,138 @@
-from tkinter import * #importa todos os módulos do tkinter
-from tkinter import messagebox #importa o módulo de caixas de mensagem
-from tkinter import ttk #importa o módulo de widgets
-from DataBase import DataBase #importa a classe de módulos DataBase
+#IMPORTAR BIBLIOTECAS
+from tkinter import* # Importa todos os módulos do tkinter
+from tkinter import messagebox # Importa o módulo de caixas de mensagem do tkinter
+from tkinter import ttk # Impottar o módulo de widgets temáticos do tkinter
+from DataBase import Database #Importa a classe Database do módulo DataBase
 
-#CRIAR JANELA
-jan = Tk() #Cria uma instância da janela principal
-jan.title('SL Systes - Painel de Acesso') #Define o título da janela
-jan.geometry('600x300') #Define o tamanho da janela
-jan.configure(background='white')
-jan.resizable(width=False, height=False) #importa a redimencionalização da janela
-jan.attributes('-alpha', 0.9) # define a tranparencia da janela (0.0 a 1.0)
-jan.iconbitmap(default='icon/ilogoIcon.ico') #define o icone da janela
-logo = PhotoImage(file='icons/LogoMatheusEduardo.png') #carrega a imagem no logo
+#CRIAR A JANELA
+jan = Tk() # Cria uma instanci da janela principal
+jan.title("SL Sytens - Painel de Acesso") #Define o título da janela
+jan.geometry("600x300") #Define o tamanho da janela
+jan.configure(background="white")#Configura a cor de fundo da janela
+jan.resizable(width=False,height=False)#Impede que a janela seja redimensionada
 
-#criar frame
-Leftframe = Frame(jan, width=200, height=300, bg='MIDNIGHTBLUE', relief='raise') #cria um frame a esquerda
-Leftframe.pack(side=LEFT)
+# COMANDO PARA DEIXAR A TELA TRANSPARENTE
+jan.attributes("-alpha",0.9) #Define a transparencia da janela (0.0 a 1.0)
 
-RightFrame = Frame(jan, width=395, height=300, bg='MIDNIGHTBLUE', relief='raise') # cria um a direita
-RightFrame.pack(side=RIGHT)
+# DEFINIR ÍCONE DA JANELA
+#jan.iconbitmap(default="icons/1LogoIcon.ico") #Define o ícone da janela
 
-#adicionar logo
-LogoLabel = Label(Leftframe, image=logo,bg='MIDNIGHTBLUE') # cria um label para a logo
-LogoLabel.place(x=50,y=100)
+# CARREGAR IMAGEM
+logo = PhotoImage(file="icons/LogoMatheusEduardo.png") #Carrega a imagem da logo
 
-#campos de usuario e senha
-UsuarioLabel = Label(RightFrame,text='Usuario: ',font=(cenrury))
+# CRIAR FRAME
+LeftFrame = Frame(jan,width=200,height=300,bg="MIDNIGHTBLUE",relief="raise") #Cria um frama à esquerda
+LeftFrame.pack(side=LEFT)#Posiciona o frama à direita
+
+RightFrame = Frame(jan,width=395,height=300,bg="MIDNIGHTBLUE",relief="raise")#Cria um frame à direita
+RightFrame.pack(side=RIGHT)#Posiciona o Frame à direita
+
+# ADICIONAR LOGO
+LogoLabel = Label(LeftFrame,image = logo,bg = "MIDNIGHTBLUE") #Cria um label para a image, do logo
+LogoLabel.place(x=50,y=100) #Posiciona o label no frama esquerdo 
+
+# ADICIONAR CAMPOS DE USUARIO E SENHA
+UsuarioLabel = Label(RightFrame,text = "Usuario: ",font=("Century Gothic",20),bg = "MIDNIGHTBLUE",fg="White")
+
+#=================================
+#AQUI PRA BAIXO
+#=================================
+
+
+# Cria um label para o usuario
+UsuarioLabel.place(x=5,y=100) #Posiciona o label no frame direito
+
+UsuarioEntry = ttk.Entry(RightFrame,width=30)#Cria um campo de entrada para o usuario
+UsuarioEntry.place(x=120,y=115) #Posiciona o campo de entrada
+
+SenhaLabel = Label(RightFrame,text="Senha: ",font=("Century Gothic",20),bg = "MIDNIGHTBLUE",fg = "White")#Cria um label pra senha
+SenhaLabel.place(x=5,y=150) #Posiciona o label no frame direito
+SenhaEntry = ttk.Entry(RightFrame,width=30,show="°") #Cria um campo de entrada para a senha
+SenhaEntry.place(x=120,y=165)#Posiciona o campo de entrada 
+
+# FUNÇÃO DE LOGIN
+def Login():
+    usuario = UsuarioEntry.get()#Obtém o valor do campo de texto de entrada do usuario
+    senha = SenhaEntry.get() #Obtém o valor do campo de entrada da senha
+
+    #Conectar o banco de dados
+    db = Database() #Cria uma instancia da classe Database
+    db.cursor.execute("""
+    SELECT*FROM usuario1 WHERE usuario = %s AND senha = %s""",(usuario,senha))#Executa a consulta SQL para verificar o usuário e a senha
+    VerifyLogin = db.cursor.fetchone() #Obtem o resultado da consulta
+
+    #Verificar se o usuario foi encontrado
+
+    if VerifyLogin:
+        messagebox.showinfo(title = "INFO LOGIN",message="Acesso Confirmado, Bem Vindo!")#Ebibe mensagem de sucesso
+        
+    else:messagebox.showinfo(title = "INFO LOGIN",message = "Acesso Negado. Verifique se está cadastrado no Sistema!")#Exibe mensagem de erro
+
+#CRIANDO BOTÕES
+LoginButton = ttk.Button(RightFrame,text="LOGIN",width=15,command=Login)#Cria um botão de login
+LoginButton.place(x=150,y=225)#Posiciona o botao de login
+
+#FUNÇÃO PARA REGISTRAR NOVO USUARIO
+def Registrar():
+    # REMOVENDO BOTOES DE LOGIN
+    LoginButton.place(x=5000)#Move o botao de login pra fora da tela
+    RegisterButton.place(x=5000)#Move o botao de registro pra fora da tela
+
+    #INSERINDO WIDGETS DE CADASTRO
+    NomeLabel = Label (RightFrame,text="Nome:",font=("Century Gothic",20),bg="MIDNIGHTBLUE",fg="White")#Cria um label para o nome
+    NomeLabel.place(x=5,y=5)#Posiciona label no frame direito
+    NomeEntry = ttk.Entry(RightFrame,width=30)#Cria um campo de entrada para o nome
+    NomeEntry.place(x=120,y=20)#Posiciona o campo de entrada
+
+    EmailLabel = Label(RightFrame,text="Email:",font=("Century Gothic",20),bg="MIDNIGHTBLUE",fg="White")#Cria um label para o email
+    EmailLabel.place(x=5,y=40)#Posiciona o label no frame direito
+    EmailEntry = ttk.Entry(RightFrame,width=30)#Cria um campo de entrada para o email
+    EmailEntry.place(x=120,y=55)#Posiciona o campo de entrada
+
+    # FUNÇÃO PARA REGISTRAR NO BANCO DE DADOS
+    def RegistrarNoBanco():
+        nome = NomeEntry.get() #Obtem o valor do campo de entrada do nome
+        email = EmailEntry.get()#Obtem o valor do campo de entrada do email
+        usuario = UsuarioEntry.get()#Obtem o valor do campo de entrada do usuario
+        senha = SenhaEntry.get()#Obtem o valor do campo de entrada da senha
+
+        #Verifa se todos os campos estao preenchidos
+        if nome == "" or email == "" or usuario == "" or senha == "":
+                messagebox.showerror(title="Erro de Registro",message="PREENCHA TODOS OS CAMPOS")#Exibe mensagem de erro 
+        else:
+            db = Database()#Cria uma instancia da classe Database
+            db.RegistrarNoBanco(nome,email,usuario,senha)#Chama o metodo para registar no banco de dados
+            messagebox.showinfo("Success","Usuario registrado com sucesso!")#Exibe mensagem de sucesso
+
+            #Limpar os campos após o registro
+            NomeEntry.delete(0,END)#Limpa o campo de entrada do Nome
+            EmailEntry.delete(0,END)#Limpa o campo de entrada do email
+            UsuarioEntry.delete(0,END)#Limpa o campo de entrada do usuario
+            SenhaEntry.delete(0,END)#Limpa o campo de entrada do senha
+
+    Register = ttk.Button(RightFrame,text="REGISTRAR",width=15,command=RegistrarNoBanco) #Cria um botão de registro
+    Register.place(x=150,y=225) #Posiciona o botão de registro
+
+    #FUNÇÃO PARA VOLTAR À TELA DE LOGIN
+    def VoltarLogin():
+        #REMOVENDO WIDGETS DE CADASTRO
+        NomeLabel.place(x=5000)#Move o label nome para fora da tela
+        NomeEntry.place(x=5000)#Move o campo de entrada do nome para fora da tela
+        EmailLabel.place(x=5000)#Move o label email para fora da tela
+        EmailEntry.place(x=5000)#Move o campo de entrada email para fora da tela
+        Register.place(x=5000)#Move o botão de registro para fora da tela
+        Voltar.place(x=5000)#Move o botão de voltar para fora da tela
+
+        #TRAZENDO DE VOLTA OS WIDGETS
+        LoginButton.place(x=150) #Traz o botão de login de volta para a tela
+        RegisterButton.place(x=150)#Traz o botão de registro de volta para a tela
+
+
+    Voltar = ttk.Button(RightFrame,text="VOLTAR",width=15,command=VoltarLogin)#Cria um botão de voltar
+    Voltar.place(x=150,y=255)#Posiciona o botão de voltar
+
+RegisterButton=ttk.Button(RightFrame,text="REGISTRAR",width=15,command=Registrar)#Cria um botão de registro
+RegisterButton.place(x=150,y=255)#Posiciona o botao de registro
+
+#INICIAR O LOOP PRINCIPAL:
+jan.mainloop()#Inicia o loop principal da aplicação
